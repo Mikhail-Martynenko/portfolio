a3fc3a7e9a49e7a0fbe187a0caa41f0cf43e057b
Mikhail-Martynenko <misha.martynenko8@yandex.ru>
2023-01-24 11:33:25 +0300

    changed css,removed button icon

--- src/components/buttons/githubButton/GithubButton.js
+++ src/components/buttons/githubButton/GithubButton.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import githubIcon from "../../../image/icons/github.svg";
-
 const ButtonGithub = ({link}) => {
     return (
         <a href={link} target="_blank" rel="noreferrer" className="button-outline">
-            <img src={githubIcon} alt="github"/>Github repository
+            Github repository
         </a>
     );
 };
